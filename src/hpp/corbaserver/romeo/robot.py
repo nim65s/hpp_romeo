bcb3bc14a967f43fdff5c41dc14c771ea9fe27d8
Florent Lamiraux <florent@laas.fr>
2020-01-08 16:07:39 +0100

    Use model of romeo in example-robot-data package.

--- src/hpp/corbaserver/romeo/robot.py
+++ src/hpp/corbaserver/romeo/robot.py
@@ -20,8 +20,8 @@ from hpp.corbaserver.robot import Robot as Parent
 class Robot (Parent):
     ##
     #  Information to retrieve urdf and srdf files.
-    urdfFilename = "package://romeo_description/urdf/romeo.urdf"
-    srdfFilename = "package://romeo_description/srdf/romeo.srdf"
+    urdfFilename = "package://example-robot-data/robots/romeo_description/urdf/romeo.urdf"
+    srdfFilename = "package://example-robot-data/robots/romeo_description/srdf/romeo.srdf"
 
     halfSitting = {'LEyePitch': 0,
                    'LWristYaw': -0.3,
