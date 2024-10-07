b3dad75e37ee4973dcbf277128916addeea145c4
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-10-07 14:30:55 +0200

    nix: temporary patches override

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
           packages = {
             default = self'.packages.hpp-romeo;
             hpp-romeo = pkgs.python3Packages.hpp-romeo.overrideAttrs (_: {
+              patches = [];
               src = pkgs.lib.fileset.toSource {
                 root = ./.;
                 fileset = pkgs.lib.fileset.unions [
